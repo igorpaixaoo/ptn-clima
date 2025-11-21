58a3201db6a2bccb10b189a3136636e52bd101ae
= <igorsantid6@gmail.com>
2025-11-21 00:06:38 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,124 @@
+var clima = document.getElementById('clima');
+var maxmin = document.getElementById('maxmin')
+var temp = document.getElementById('temp');
+var humidity = document.getElementById('humidity');
+var nascerDoSol = document.getElementById('nascer-do-sol')
+var chanceChuva = document.getElementById('chance-chuva')
+var porDoSol = document.getElementById('por-do-sol')
+var vento = document.getElementById('vento')
+
+var remetenteAlert = document.getElementById('remetente')
+var eventoAlert = document.getElementById('evento')
+var descricaoAlert = document.getElementById('descricao')
+var duracaoAlert = document.getElementById('duracao')
+
+var dia1 = document.getElementById("dia1"), dia2 = document.getElementById("dia2"), dia3 = document.getElementById("dia3"), dia4 = document.getElementById("dia4"), dia5 = document.getElementById("dia5"), dia6 = document.getElementById("dia6"), dia7 = document.getElementById("dia7")
+var chanceChuvaDia1 = document.getElementById("chance-chuva-dia1"), chanceChuvaDia2 = document.getElementById("chance-chuva-dia2"), chanceChuvaDia3 = document.getElementById("chance-chuva-dia3"),chanceChuvaDia4 = document.getElementById("chance-chuva-dia4"),chanceChuvaDia5 = document.getElementById("chance-chuva-dia5"),chanceChuvaDia6 = document.getElementById("chance-chuva-dia6"),chanceChuvaDia7 = document.getElementById("chance-chuva-dia7")
+
+const dias = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"]
+const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Ago", "Set", "Out", "Nov", "Dez"]
+
+async function getDataPtn() {
+    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=-13.4557&lon=-39.4212&units=metric&appid=5796abbde9106b7da4febfae8c44c232&lang=pt_br";
+
+    const url2 = "https://api.openweathermap.org/data/2.5/weather?lat=-13.4557&lon=-39.4212&units=metric&appid=5796abbde9106b7da4febfae8c44c232&lang=pt_br"
+
+
+    const response = await fetch(url2);
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    maxmin.innerText = Math.round(result.main.temp_min) + " / " + Math.round(result.main.temp_max) + "ºC"
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        var climaValue = result.current.weather[0].description
+        clima.innerText = climaValue
+
+        var tempValue = result.current.temp
+        temp.innerText = Math.round(tempValue) + "ºC";
+
+        var humidityValue = result.current.humidity
+        humidity.innerText = "Umidade: " + humidityValue + "%"
+
+        var nascerDoSolValue = new Date(result.current.sunrise * 1000);
+        nascerDoSol.innerText = nascerDoSolValue.getHours() + "h" + ":" + nascerDoSolValue.getMinutes() + "min";
+
+        var porDoSolValue = new Date(result.current.sunset * 1000);
+        porDoSol.innerText = porDoSolValue.getHours() + "h" + ":" + porDoSolValue.getMinutes() + "min";
+
+        
+        var ventoValue = result.current.wind_speed
+        vento.innerText = "Vento: " + ventoValue + "m/s";
+
+        //var remetenteValue = result.alerts[0].sender_name
+        //remetenteAlert.innerText = remetenteValue
+
+        
+        var eventoValue = result.alerts[0].event
+
+        // se o evento retornar nulo, <div> alert é desativada
+        if(eventoValue != null){
+            eventoAlert.innerText = "Alerta de " + eventoValue
+
+            var descricaoValue = result.alerts[0].description
+            descricaoAlert.innerText = descricaoValue
+
+            var inicioAlert = new Date(result.alerts[0].start * 1000)
+            var fimAlert = new Date(result.alerts[0].end * 1000)
+
+            duracaoAlert.innerText = "Entre " + inicioAlert.toLocaleDateString() + " das " + inicioAlert.toLocaleTimeString() + " até o dia " + fimAlert.toLocaleDateString() + " às " + fimAlert.toLocaleTimeString();
+        }else document.getElementById('alerts').style.display = "none"
+
+ 
+
+        //chance de chuva para o dia atual, pega o primeiro objeto .daily
+        chanceChuva.innerText = "Chuva: " + (result.daily[0].pop * 100) + "%"
+
+        var dia1Value = new Date(result.daily[1].dt * 1000)
+        dia1.innerText = dias[dia1Value.getDay()] + ", " + dia1Value.getDate() + "/" + dia1Value.getMonth() + "/" + dia1Value.getFullYear()
+
+        var dia2Value = new Date(result.daily[2].dt * 1000)
+        dia2.innerText = dias[dia2Value.getDay()] + ", " + dia2Value.getDate() + "/" + dia2Value.getMonth() + "/" + dia2Value.getFullYear()
+
+        var dia3Value = new Date(result.daily[3].dt * 1000)
+        dia3.innerText = dias[dia3Value.getDay()] + ", " + dia3Value.getDate() + "/" + dia3Value.getMonth() + "/" + dia3Value.getFullYear()
+
+        var dia4Value = new Date(result.daily[4].dt * 1000)
+        dia4.innerText = dias[dia4Value.getDay()] + ", " + dia4Value.getDate() + "/" + dia4Value.getMonth() + "/" + dia4Value.getFullYear()
+
+        var dia5Value = new Date(result.daily[5].dt * 1000)
+        dia5.innerText = dias[dia5Value.getDay()] + ", " + dia5Value.getDate() + "/" + dia5Value.getMonth() + "/" + dia5Value.getFullYear()
+
+        var dia6Value = new Date(result.daily[6].dt * 1000)
+        dia6.innerText = dias[dia6Value.getDay()] + ", " + dia6Value.getDate() + "/" + dia6Value.getMonth() + "/" + dia6Value.getFullYear()
+
+        var dia7Value = new Date(result.daily[7].dt * 1000)
+        dia7.innerText = dias[dia7Value.getDay()] + ", " + dia7Value.getDate() + "/" + dia7Value.getMonth() + "/" + dia7Value.getFullYear()
+
+        chanceChuvaDia1.innerText = Math.round((result.daily[1].pop) * 100) + "%" + " chance de chuva"
+        chanceChuvaDia2.innerText = Math.round((result.daily[2].pop) * 100) + "%" + " chance de chuva"
+        chanceChuvaDia3.innerText = Math.round((result.daily[3].pop) * 100) + "%" + " chance de chuva"
+        chanceChuvaDia4.innerText = Math.round((result.daily[4].pop)) * 100 + "%" + " chance de chuva"
+        chanceChuvaDia5.innerText = Math.round((result.daily[5].pop) * 100) + "%" + " chance de chuva"
+        chanceChuvaDia6.innerText = Math.round((result.daily[6].pop)) * 100 + "%" + " chance de chuva"
+        chanceChuvaDia7.innerText = Math.round((result.daily[7].pop)) * 100 + "%" + " chance de chuva"
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getDataPtn()
+
+//atualizar a cada 3 minutos
+setInterval(getDataPtn, 60000*3)
+
